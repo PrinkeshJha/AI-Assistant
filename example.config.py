489f9beaa8e6a45286c43b87070871c488d3dbde
PrinkeshJha <jhaprinkesh@gmail.com>
2025-09-16 13:54:10 +0530

    updated example.config.py

---
+++ example.config.py
@@ -0,0 +1,17 @@
+# # Give this script name as config.py
+
+
+# --- API Keys ---
+# Get from https://newsapi.org/
+NEWS_API_KEY = "YOUR API KEY"
+# Get from https://openweathermap.org/
+OPENWEATHER_API_KEY = "YOUR API KEY"
+# Get from https://console.picovoice.ai/
+PICOVOICE_ACCESS_KEY = "YOUR ACCESS KEY"
+
+# --- Assistant Configuration ---
+WAKE_WORD = "jarvis"
+ASSISTANT_NAME = "Jarvis"
+
+
+MICROPHONE_INDEX = None
